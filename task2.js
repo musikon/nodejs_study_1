a1c5320da576e56f1b16572fe6259e3a029d0ea3
Yurii_Kosygin <yurri.kosygin@ukg.com>
2022-12-01 11:24:12 +0300

    add function

--- task2.js
+++ task2.js
@@ -1,9 +1,13 @@
+import { readFile } from 'node:fs/promises'
 import { createWriteStream, createReadStream } from 'node:fs'
 import csv from 'csvtojson'
 
-const csvFilePath = './assets/book_test.csv'
+const csvFilePathBig = './assets/book_test.csv'
+const csvFilePathSmall = './assets/books.csv'
 const writeStream = createWriteStream('./new_file.txt')
-const readStream = createReadStream(csvFilePath, 'utf8')
+
+const writable = createWriteStream('./new_file.txt')
+const readStream = createReadStream(csvFilePathBig, 'utf8')
 
 const options = {
   delimiter: ';',
@@ -23,8 +27,29 @@ const writeFile = () => {
   writeStream.on('finish', () =>
     console.log(`End write, time: ${Date.now() - startTime}ms`),
   )
-
   readStream.pipe(csv(options)).pipe(writeStream)
 }
-
 writeFile()
+
+/*
+ // second solve
+const writeItems = (writer, data, encoding) => {
+  let i = 0
+  let ok = true
+  do {
+    if (i === data.length - 1) {
+      writer.write(JSON.stringify(data[i]), encoding)
+      return 'File upload'
+    }
+    ok = writer.write(`${JSON.stringify(data[i])}\n`, encoding)
+
+    i++
+  } while (i < data.length && ok)
+  return 'Error'
+}
+
+readFile(csvFilePathSmall)
+  .then((data) => csv(options).fromString(data.toString()))
+  .then((jsonObj) => writeItems(writable, jsonObj, 'utf8'))
+  .then(console.log)
+  .catch(console.error) */
