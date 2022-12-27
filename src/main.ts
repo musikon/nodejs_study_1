03b5336d58de2103081ec1cf3009279074e6ab79
Yurii_Kosygin <ykosygin@gmail.com>
2022-12-28 00:27:28 +0300

    first

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import * as dotenv from 'dotenv'
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+dotenv.config()
+
+async function start() {
+  const PORT = process.env.PORT || 8000
+  const app = await NestFactory.create(AppModule)
+
+  await app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`))
+}
+start();
